0479a0728646b776b69188546fd9689fe133fd81
dejoeypoe <109167180+dejoeypoe@users.noreply.github.com>
2022-07-13 00:05:45 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,24 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
-*♂️ Gender* : Laki - laki
+*💌 Nama* : Joey
+*✉️ Nama RL* : Zoe
+*♂️ Gender* : Male
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*⏰ Tanggal lahir* : 5 Dec
+*🎨 Umur* : -
+*🧮 Status* : Mahasiswa
+*🧩 Hobby* : Ngegame, Nonton anime, Ngodeng, Desain, Bikin Art
+*💬 Sifat* : 999+
+*🗺️ Alamat* : Surakarta, Jawa Tengah, Indonesia
+*❤️ Suka* : Hitam, kucing, kamu
+*💔 Benci* : autis, anak epep, seleb, so keren
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*📷 instagran* : @zoex.yz
+*🇫  Facebook* : Mythicaly Myth
+*🏮 Chanel Youtube* : Sementara Belom Ada
+*🐈 Github:* dejoeypoe
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
