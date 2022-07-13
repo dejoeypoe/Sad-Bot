d73dd8af9cfa367dab088e477599ff9460b08010
dejoeypoe <109167180+dejoeypoe@users.noreply.github.com>
2022-07-13 14:43:19 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ᴊᴏᴇʏ-ʙᴏᴛ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ ᴘᴀᴘᴀʜ-DJOEY,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://www.instagram.com/zoex.yz/', 'IG ᴏᴡɴᴇʀ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
+[`\nɢᴡᴇʜ ɢᴇɪ`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
